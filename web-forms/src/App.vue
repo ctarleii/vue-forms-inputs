8e509a63e49d521bcddb395450091c7fde5a303a
ctarleii <egorcerencov0023@gmail.com>
2023-08-24 12:00:46 +1000

    commit 1.4

--- web-forms/src/App.vue
+++ web-forms/src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SignupForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupForm from './components/SignupForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      SignupForm,
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +23,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body {
+    margin: 0;
+    background: #eee;
+}
+
+
 </style>
